9bffc0641e2744dcc0e4dcae7e74e90f6dfb6859
Igor-R64 <70832743+Igor-R64@users.noreply.github.com>
2021-07-12 18:29:55 +0400

    Revert "Delete frontend directory"
    This reverts commit 75743bfc9e4586c214e34c99666f6dee7952c720.

---
+++ frontend/src/components/Contacts/Contacts.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { YMaps, Map } from 'react-yandex-maps';
+import { FaTelegram, FaInstagram, FaFacebook } from 'react-icons/fa';
+import { Row, Col, Toast, ToastBody, ToastHeader } from 'reactstrap';
+
+const mapState = { center: [51.53, 46.03], zoom: 13 };
+
+class Contacts extends React.Component {
+  state = { showMap: true };
+
+  toggleMap() {
+    const { showMap } = this.state;
+    this.setState({ showMap: !showMap });
+  }
+
+  render() {
+    const { showMap } = this.state;
+
+    return (
+
+      <div>
+
+        <div className="p-3 bg-secondary my-2 rounded">
+
+          <Row>
+            <Col xs="6">
+              <Toast>
+                <ToastHeader>
+                  Контакты
+                </ToastHeader>
+                <ToastBody>
+                  <p >
+                    Тел: 8 800 000 000
+                  </p>
+                  <p>
+                    mail: saratov@saratov.ru
+                  </p>
+                  <p>
+                    Адрес: ул. Сакко и Ванцетти д. 00, Саратов, Россия
+                  </p>
+                </ToastBody>
+              </Toast>
+            </Col>
+            <Col xs="6">
+              <Toast>
+                <ToastHeader>
+                  Мы в социальных сетях
+                </ToastHeader>
+                <ToastBody>
+                  <p href="#" >
+                    <FaTelegram /> telegram
+                  </p>
+                  <p>
+                    <FaInstagram /> instagram
+                  </p>
+                  <p>
+                    <FaFacebook /> facebook
+                  </p>
+                </ToastBody>
+              </Toast>
+            </Col>
+          </Row>
+
+        </div>
+
+
+
+
+
+        <YMaps>
+          <div id="map-basics">
+            {showMap &&
+              <Map
+                width='1000px'
+                height='500px'
+                state={mapState} />}
+          </div>
+        </YMaps>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
